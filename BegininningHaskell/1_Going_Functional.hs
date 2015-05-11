109e27965aaa97f2c41c6dd264e61738882eaaae
Adriel Bergmann <adrielb@users.noreply.github.com>
2015-05-11 15:17:13 -0400

    1: Beginning Haskel, Going Functional

---
+++ BegininningHaskell/1_Going_Functional.hs
@@ -0,0 +1,25 @@
+-- 1 Going Functional
+
+-- purity - separates code with side effects from the rest
+-- lazy evaluation model
+-- compiler statically checks types
+-- polymorphism by parametricity and type classes
+
+-- functional - functions are first class citizens, 
+--              manipulated like any other data in a program
+-- imperative - methods that consists of statements that modify a global state
+
+-- Type system - restricts possible actions that could be applied to a value
+-- dynamic typing - checking types at execution time
+-- static typing - checking types at the moment of compilation
+
+-- interactive multiline block:
+:{
+    1/3 + 
+    1/2
+:}
+
+-- internal actions of the interpreter
+:?
+
+:show language
