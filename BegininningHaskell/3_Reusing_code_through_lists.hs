3fe2db3541ff6dc9a26ff24a1b8d6390fb99b04b
Adriel Bergmann <adrielb@users.noreply.github.com>
2015-05-28 13:08:19 -0400

    Param polymorphism, Funcs as params, anon funcs

---
+++ BegininningHaskell/3_Reusing_code_through_lists.hs
@@ -0,0 +1,132 @@
+-- Parametric Polymorphism
+-- head is parametric polymorphic
+:t head
+
+:t head "Hello"
+
+:t fst
+
+:t ([3,4], "hello")
+
+let maybeString (Just _) = "Just"
+let maybeString Nothing  = "Nothing"
+
+:t maybeString
+
+maybeString Nothing
+
+data Triple a b c = Triple a b c deriving Show
+
+:t Triple "a" 3 3.4
+
+-- type name    = constructor name
+data SamePair a = SamePair a a deriving Show
+
+:t SamePair 3 4
+
+let swapTriple (x,y,z) = (y,z,x)
+
+:t swapTriple
+
+let duplicate x = (x,x)
+
+:t duplicate
+
+let nothing _ = Nothing
+
+:t nothing
+
+let index [] = []
+let index [x] = [(0,x)]
+let index (x:xs) = let indexed@((n,_):_) = index xs
+                    in (n+1, x):indexed
+
+index []
+
+:t index
+
+let maybeA [] = 'a'
+
+:t maybeA
+
+--
+-- Functions as Parameters
+succ 1
+
+map succ [1,2,3]
+
+:t map
+
+apply3f2 :: (Integer ->Integer) -> Integer -> Integer
+
+let apply3f2 f x = 3 * f (x + 2)
+
+apply3f2 succ 7
+
+:t ($)
+
+maximum (map succ [1,2,3])
+
+maximum $ map succ [1,2,3]
+
+--
+-- Anonymous Functions
+-- \param1 param2 ... -> body
+
+map (\x -> x + 2) [1,2,3]
+
+equalTuples :: [(Integer, Integer)] -> [Bool]
+
+let equalTuples t = map (\(x,y) -> x == y) t
+
+equalTuples [(1,2),(3,3), (5,4)]
+
+-- anon functions dont have a name, forbidding recursion
+-- must use 'case' for pattern matching
+sayHello :: [String] -> [String]
+
+let sayHello names = map (\name -> case name of 
+                     "Alejandro" -> "Hello, writer"
+                     _           -> "Welcome, " ++ name
+                     ) names
+
+sayHello ["Max", "Alejandro", "Jack"]
+
+-- special syntax to match cases for only one parameter
+:set -XLambdaCase
+
+{-# LANGUAGE LambdaCase #-}
+
+let sayHello names = map (\case "Alejandro" -> "hello, writer"
+                            name -> "Welcom, " -> name
+                     ) names
+
+-- closures
+let multiplyByN n = \x -> n*x
+
+map (multiplyByN 4) [4,3,2]
+
+:t filter
+
+filter even [1,2,3,4,5]
+
+let lst = [1,2,3,1,5]
+
+let filterOnes x = x == 1
+
+filter filterOnes lst
+
+let filterANumber n = \x -> x == n
+
+filter (filterANumber 5) lst
+
+let filterNot f lst = filter (\x -> not $ f x) lst
+
+let filterNot f lst = filter (not . f) lst
+
+filterNot filterOnes lst
+
+
+
+
+
