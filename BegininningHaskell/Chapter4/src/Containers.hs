eb7b4f8512ded5896a3cb93646ff5533647bf11a
Adriel Bergmann <adrielb@users.noreply.github.com>
2015-06-12 17:33:17 -0400

    BH: Graphs

--- BegininningHaskell/Chapter4/src/Containers.hs
+++ BegininningHaskell/Chapter4/src/Containers.hs
@@ -3,6 +3,7 @@ module Containers where
 import qualified Data.Map as M
 import qualified Data.Set as S
 import Data.Tree
+import Data.Graph
 
 data Client i = GovOrg  { clientId :: i, clientName :: String }
               | Company { clientId :: i, clientName :: String
@@ -28,3 +29,19 @@ pictureTree = Node 1 [ Node 2 [ Node 3 []
                               , Node 4 []
                               , Node 5 [] ]
                      , Node 6 [] ]
+
+
+-- Graphs
+timeMachineGraph :: [(String, String, [String])]
+timeMachineGraph = 
+  [("wood","wood",["walls"]), ("plastic","plastic",["walls","wheels"])
+  ,("aluminum","aluminum",["wheels","door"]),("walls","walls",["done"])
+  ,("wheels","wheels",["done"]),("door","door",["done"]),("done","done",[])]
+
+timeMachinePrecedence :: (Graph, Vertex -> (String, String, [String]), String -> Maybe Vertex)
+timeMachinePrecedence = graphFromEdges timeMachineGraph
+
+timeMachineTravel :: Graph
+timeMachineTravel = buildG (103,2013)
+                      [(1302,1614),(1614,1302),(1302,2013),(2013,1302),(1614,2013)
+                      ,(2013,1408),(1408,1993),(1408,917),(1993,917),(907,103),(103,917)]
