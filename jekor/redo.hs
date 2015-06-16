34c0456204ed0805ee263d9f1189738bdb16598d
Adriel Bergmann <adrielb@users.noreply.github.com>
2015-06-16 15:45:37 -0400

    added traceShow' to print and pass on args

--- jekor/redo.hs
+++ jekor/redo.hs
@@ -1,12 +1,14 @@
 import Control.Monad (filterM, liftM)
 import Data.Maybe (listToMaybe)
+import Debug.Trace (trace, traceShow)
 import System.Directory (renameFile,removeFile,doesFileExist)
 import System.Environment (getArgs)
 import System.Exit (ExitCode(..))
 import System.FilePath (hasExtension, replaceBaseName, takeBaseName)
 import System.IO (hPutStrLn, stderr)
 import System.Process (createProcess, waitForProcess, shell)
-import Debug.Trace
+
+traceShow' arg = traceShow arg arg
 
 main :: IO ()
 main = mapM_ redo =<< getArgs
@@ -18,13 +20,13 @@ redo target = maybe printMissing redo' =<< redoPath target
         redo' path = do
             (_,_,_,ph) <- createProcess $ shell $ cmd $ path
             exit <- waitForProcess ph 
-            case exit of
+            case traceShow' exit of
                 ExitSuccess      -> do renameFile tmp target
                 ExitFailure code -> do hPutStrLn stderr $ "Redo exited: " ++ show code
                                        removeFile tmp
         tmp = target ++ "---redoing"
         printMissing = (error $ "No .do file found for target " ++ target)
-        cmd path = unwords ["sh",path,"0",takeBaseName target,tmp,">",tmp]
+        cmd path = traceShow' $ unwords ["sh",path,"0",takeBaseName target,tmp,">",tmp]
 
 redoPath :: FilePath -> IO (Maybe FilePath)
 redoPath target = listToMaybe `liftM` filterM doesFileExist candidates
