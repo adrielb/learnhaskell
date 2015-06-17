61d3b563213e1d72c077df6d7826f99d106cc2da
Adriel Bergmann <adrielb@users.noreply.github.com>
2015-06-17 17:41:15 -0400

    redo exception handling

--- jekor/build.sh
+++ jekor/build.sh
@@ -1,3 +1,2 @@
-redo-ifchange redo.hs README.txt
 packages=/home/abergman/projects/learnhaskell/jekor/.cabal-sandbox/x86_64-linux-ghc-7.8.4-packages.conf.d
-ghc -v0 -package-db $packages -o $3 redo.hs
+ghc -v0 -package-db $packages -o redo redo.hs
--- jekor/local.vimrc
+++ jekor/local.vimrc
@@ -1,1 +1,9 @@
 set makeprg=./redo\ redo
+
+function! BuildRedo()
+	set makeprg=./build.sh
+	exec ":make"
+	set makeprg=./redo\ redo
+endfunction
+
+noremap <leader>b :call BuildRedo()<CR>
